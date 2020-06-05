ccbc15fb83ad129afc93802b7c4616f175639bcf
fiona <1125575769@qq.com>
2020-06-05 14:03:52 +0800

    lesson 1 update

--- lesson_1/index.js
+++ lesson_1/index.js
@@ -1,12 +1,12 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-class App extends Component{
-    render(){
-        return(
-            <div>
-                App
-            </div>
+class App extends Component {
+    render() {
+        return ( <
+            div >
+            App <
+            /div>
         )
     }
 }
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
